416f85dd106c12451a91cc1433b6acc314500703
Saad Niazi <saadniazifed@gmail.com>
2022-07-31 19:44:56 +0500

    Added Cars in the Shop NavLink.
    Worked on adding the cars in the Shop NavLink. No State has been added yet.

--- src/components/Shop/ShopItems/ShopItems.js
+++ src/components/Shop/ShopItems/ShopItems.js
@@ -1,5 +1,23 @@
 import React from "react";
+import cars from "../../../carData/carData";
+import "./ShopItems.css";
 
 export const ShopItems = () => {
-  return <div>ShopItems</div>;
+  return (
+    <div className="shopping--wrapper">
+      {cars.map((car) => {
+        return (
+          <div className="shopping--container">
+            <img src={car.image} alt="super car" />
+            <div className="car--details">
+              <p className="carName">{car.name}</p>
+              <p className="titilliumFont">{car.description}</p>
+              <p className="titilliumFont">Price: ${car.price}</p>
+            </div>
+            <div className="marginBottom"></div>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
