77a204a7676825ed8ed505e6077ff496c6a1b63c
Saad Niazi <saadniazifed@gmail.com>
2022-07-31 13:32:04 +0500

    Added the video and text on the homepage.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,29 @@
 import React from "react";
+import video from "../../logo/ferrari_homepage_video.mp4";
+import "./Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="ferrari--homepage">
+      <video
+        style={{
+          objectFit: "cover",
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          top: "0",
+          left: "0",
+          zIndex: "-1",
+        }}
+        playsInline
+        autoPlay
+        muted
+        loop
+        id="background-video"
+      >
+        <source src={video} type="video/mp4"></source>
+      </video>
+      <h1 className="ferrari--text">Legend is Reborn.</h1>
+    </div>
+  );
 };
