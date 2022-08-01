e849eb580977ef560b1d75ddd995fddd2daef53b
Saad Niazi <saadniazifed@gmail.com>
2022-08-01 23:27:12 +0500

    Pushing the latest changes for the day.
    A small logic error is coming. Hopefully will be able to figure it out tomorrow.

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,32 +1,20 @@
 import React, { useState } from "react";
-import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
+import carsData from "../../carData/carData";
 import { ShopItems } from "./ShopItems/ShopItems";
-import "./Shop.css";
 
 export const Shop = () => {
-  const [cars, setCars] = useState(0);
-  const [addCart, setAddCart] = useState(false);
-
-  const increaseCars = () => {
-    if (cars < 10) {
-      setCars((prevCars) => prevCars + 1);
-    }
-  };
-
-  const decreaseCars = () => {
-    if (cars > 0) {
-      setCars((prevCars) => prevCars - 1);
-    }
-  };
+  const [cars, setCars] = useState(carsData);
+  const [carAmount, setCarAmount] = useState();
+  // const [cartList, setCartList] = useState([]); <--Will use it later for Adding it to Cart.
 
   return (
-    <div className="shop--container">
+    <>
       <ShopItems
-        increaseCars={increaseCars}
-        decreaseCars={decreaseCars}
-        carsQuantity={cars}
+        cars={cars}
+        carAmount={carAmount}
+        setCarAmount={setCarAmount}
+        // addToCart={addToCart}
       />
-      <ShoppingCart decreaseCars={decreaseCars} cars={cars} />
-    </div>
+    </>
   );
 };
--- src/components/Shop/ShopItems/ShopItems.js
+++ src/components/Shop/ShopItems/ShopItems.js
@@ -1,39 +1,43 @@
-import React, { useState } from "react";
-import cars from "../../../carData/carData";
+import React from "react";
 import "./ShopItems.css";
 
-export const ShopItems = (props) => {
-  const { carsQuantity, increaseCars, decreaseCars } = props;
-
+export function ShopItems({ cars }) {
   return (
-    <div className="shopping--wrapper">
-      {cars.map((car) => {
-        return (
-          <div className="shopping--container">
-            <img src={car.image} alt="super car" />
-            <div className="car--details ">
-              <p className="carName">{car.name}</p>
-              <p className="titilliumFont">{car.description}</p>
-              <p className="titilliumFont">Price: ${car.price}</p>
+    <>
+      <div className="shop--container">
+        <div className="shopping--wrapper">
+          {cars.map((car) => {
+            return (
+              <div className="shopping--container" key={car.id} id={car.id}>
+                <img src={car.image} alt="super car" />
+                <div className="car--details ">
+                  <p className="carName">{car.name}</p>
+                  <p className="titilliumFont">{car.description}</p>
+                  <p className="titilliumFont">Price: ${car.price}</p>
 
-              <div className="car--quantity--container">
-                <button type="button" onClick={decreaseCars}>
-                  -
-                </button>
-                <div className="car--quantity">{carsQuantity}</div>
-                <button type="button" onClick={increaseCars}>
-                  +
-                </button>
+                  <input
+                    type="number"
+                    className="car--quantity--input"
+                    // onChange={(e) => changeCarAmount(e)}
+                    // value={carAmount}
+                    name="cars"
+                    min="1"
+                    max="10"
+                  />
+                  <button
+                    type="button"
+                    className="addToCart"
+                    // onClick={(e) => addToCart(e, cars)}
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+                <div className="marginBottom"></div>
               </div>
-
-              <button type="button" className="addToCart">
-                Add to Cart
-              </button>
-            </div>
-            <div className="marginBottom"></div>
-          </div>
-        );
-      })}
-    </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
-};
+}
