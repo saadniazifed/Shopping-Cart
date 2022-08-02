3658134a541a77974e0e4dbb601699eebeee5ad5
Saad Niazi <saadniazifed@gmail.com>
2022-08-02 22:35:07 +0500

    Quantities and the cars are being added successfully in the shopping cart.

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,56 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import carsData from "../../carData/carData";
 import { ShopItem } from "./ShopItem/ShopItem";
 import { ShoppingCart } from "./ShoppingCart/ShoppingCart";
 import "./Shop.css";
 
 export const Shop = () => {
-  const [cars, setCars] = useState(carsData);
   const [cart, setCart] = useState([]);
-  const [cartLength, setCartLength] = useState(cart.length);
 
-  const renderShopItems = () => {
-    return cars.map((car) => {
-      return (
-        <ShopItem
-          cart={cart}
-          car={car}
-          key={car.id}
-          changeCarAmount={changeCarAmount}
-          addToCart={addToCart}
-        />
-      );
-    });
-  };
-
-  const addToCart = (e, quantity, name) => {
-    e.preventDefault();
-
+  const addToCart = (quantity, name) => {
     const cartObj = {
-      quantity: quantity.cart,
-      name: name,
+      quantity,
+      name,
     };
-
-    setCart([
-      ...cart,
-      {
-        cart: [...cart, cartObj],
-      },
-    ]);
-
-    console.log(cart);
+    setCart((prev) => [...prev, cartObj]);
   };
-
-  const changeCarAmount = (e) => {
-    setCart(e.target.value);
-  };
+  console.log(cart);
 
   return (
     <>
       <div className="shop--container">
         <div className="shopping--wrapper">
-          {renderShopItems()}
-          <ShoppingCart cartLength={cartLength} />
+          {carsData.map((car) => (
+            <ShopItem key={car.id} car={car} addToCart={addToCart} />
+          ))}
+          <ShoppingCart cartLength={cart.length} />
         </div>
       </div>
     </>
--- src/components/Shop/ShopItem/ShopItem.js
+++ src/components/Shop/ShopItem/ShopItem.js
@@ -1,37 +1,37 @@
 import React from "react";
+import { useState } from "react";
 import "./ShopItem.css";
 
 export function ShopItem(props) {
-  const { car, addToCart, cart, changeCarAmount } = props;
+  const { car, addToCart } = props;
+  const [itemQuantity, setItemQuantity] = useState(1);
 
   return (
-    <>
-      <div className="shopping--container" key={car.id} id={car.id}>
-        <img src={car.image} alt="super car" />
-        <div className="car--details ">
-          <p className="carName">{car.name}</p>
-          <p className="titilliumFont">{car.description}</p>
-          <p className="titilliumFont">Price: ${car.price}</p>
+    <div className="shopping--container" key={car.id} id={car.id}>
+      <img src={car.image} alt="super car" />
+      <div className="car--details ">
+        <p className="carName">{car.name}</p>
+        <p className="titilliumFont">{car.description}</p>
+        <p className="titilliumFont">Price: ${car.price}</p>
 
-          <input
-            type="number"
-            className="car--quantity--input"
-            onChange={(e) => changeCarAmount(e)}
-            value={cart}
-            name={car.name}
-            min="1"
-            max="10"
-          />
-          <button
-            type="button"
-            className="addToCart"
-            onClick={(e) => addToCart(e, { cart }, car.name)}
-          >
-            Add to Cart
-          </button>
-        </div>
-        <div className="marginBottom"></div>
+        <input
+          type="number"
+          className="car--quantity--input"
+          onChange={(e) => setItemQuantity(e.target.value)}
+          value={itemQuantity}
+          name={car.name}
+          min="1"
+          max="10"
+        />
+        <button
+          type="button"
+          className="addToCart"
+          onClick={(e) => addToCart(itemQuantity, car.name)}
+        >
+          Add to Cart
+        </button>
       </div>
-    </>
+      <div className="marginBottom"></div>
+    </div>
   );
 }
